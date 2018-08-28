a375e045e9d07002052581808aafb0cbfa0bb428
Vladimir Budilov <odessitv@gmail.com>
2018-08-28 19:28:33 -0400

    Initial working version

---
+++ handler.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports.confirm = (event, context, callback) => {
+  console.log('Received event:', JSON.stringify(event, null, 2))
+  const modifiedEvent = event
+  
+  // check that we're acting on the right trigger
+  if (event.triggerSource === "PreSignUp_SignUp"){
+    
+    // auto confirm the user
+    modifiedEvent.response.autoConfirmUser = true
+    
+    callback(null, modifiedEvent)
+    return
+  }
+  
+  // Throw an error if invoked from the wrong trigger
+  callback(`Misconfigured Cognito Trigger ${ event.triggerSource }`)
+};
